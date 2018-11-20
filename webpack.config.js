3a2f91ec3ee6342e5f615b3b6ee81f9a02c2dbd1
qijunyao <qijunyao@xiaohongshu.com>
2018-11-20 15:13:02 +0800

    init all content

--- webpack.config.js
+++ webpack.config.js
@@ -15,13 +15,13 @@ const { VueLoaderPlugin } = require('vue-loader');
 module.exports = {
   // mode: 'development',  // 指定mode, WARN 实践 通过命令行指定
   entry: {
-    // main: '@/index.js'
+    main: '@/index_basic.js'
     // main: '@/vue_main.js'
     // main: '@/vue_router.js'
     // main: '@/vuex_index.js'
     // main: '@/vue_component_main.js'
     // main: '@/axios_main.js'
-    main: '@/element_ui_demo.js'
+    // main: '@/element_ui_demo.js'
   },
   output: {
     path: DIST_PATH,
@@ -76,7 +76,7 @@ module.exports = {
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: path.resolve(TEM_PATH, 'element_ui_demo.html')
+      template: path.resolve(TEM_PATH, 'index_basic.html')
     }),
     new VueLoaderPlugin()
   ]
